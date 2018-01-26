f01efa64984164966e7fe038516e0b0be9ba9773
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-26 18:28:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,19 @@
 require 'pry'
 require_relative './concerns/memorable.rb'
+require_relative './concerns/findable.rb'
 
 
 class Artist
   extend Memorable::ClassMethods
-
+  extend Findable::ClassMethods
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def initialize
     @@artists << self
--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,7 @@
 module Findable
 
   module ClassMethods
-    
+
     def find_by_name(name)
       class.detect {|person| person.name = name}
       end
