7135a5eabc91336c29cad7702adeea5f149702ed
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-26 18:37:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,12 @@
 require 'pry'
 require_relative'./concerns/memorable.rb'
 require_relative './concerns/findable.rb'
+require_relative './concerns/paramable.rb'
 class Song
 
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
+  extend Paramable::ClassMethods
 
   attr_accessor :name
   attr_reader :artist
