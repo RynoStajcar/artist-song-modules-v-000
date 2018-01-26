49839d003ab4776e7b622740f749a63053660c49
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-26 18:30:29 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,7 +3,7 @@ module Findable
   module ClassMethods
 
     def find_by_name(name)
-      class.detect {|person| person.name = name}
+      self.detect {|person| person.name = name}
       end
   end
 end
